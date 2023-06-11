bedb02b461913565abd4d7239740053b1e7a27fa
br3nr <repetitive_routine@protonmail.com>
2023-06-11 18:31:20 +0800

    set camera to correct position

--- frontend/src/components/Scene/Scene.tsx
+++ frontend/src/components/Scene/Scene.tsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Keyboard from "../Keyboard/Keyboard";
+import { PerspectiveCamera } from "three";
+import { useRef } from "react";
+import { CameraControls } from '@react-three/drei';
 
 export default function Scene() {
   return (
     <>
-      <Canvas>
-        <OrbitControls />
+      <Canvas camera={{fov:40, position:[0,0,20]}}>
+        <OrbitControls/>  
         <ambientLight intensity={0.3} position={[0, -0, -5]} />
         <spotLight position={[0, -0, 100]} angle={0.3} />
         <Keyboard />
