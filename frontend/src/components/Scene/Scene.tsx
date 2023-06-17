b17d9b631e0564d3433d6c305c9d2ba574b0736c
br3nr <repetitive_routine@protonmail.com>
2023-06-17 19:03:18 +0800

    component testing

--- frontend/src/components/Scene/Scene.tsx
+++ frontend/src/components/Scene/Scene.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Keyboard from "../Keyboard/Keyboard";
+import Spacebar from "../Spacebar/Spacebar";
 import { PerspectiveCamera } from "three";
 import { useRef } from "react";
 import { CameraControls } from '@react-three/drei';
@@ -14,6 +15,7 @@ export default function Scene() {
         <ambientLight intensity={0.3} position={[0, -0, -5]} />
         <spotLight position={[0, -0, 100]} angle={0.3} />
         <Keyboard />
+        <Spacebar />
       </Canvas>
     </>
   );
