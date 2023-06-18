99fca252a9eacbddfbd81f58b804114bd4fe2ab4
br3nr <repetitive_routine@protonmail.com>
2023-06-18 21:01:03 +0800

    Design all keycap sizes from default 1u

--- frontend/src/components/Scene/Scene.tsx
+++ frontend/src/components/Scene/Scene.tsx
@@ -1,11 +1,6 @@
-import { useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Keyboard from "../Keyboard/Keyboard";
-import Spacebar from "../Spacebar/Spacebar";
-import { PerspectiveCamera } from "three";
-import { useRef } from "react";
-import { CameraControls } from '@react-three/drei';
 
 export default function Scene() {
   return (
@@ -15,7 +10,6 @@ export default function Scene() {
         <ambientLight intensity={0.3} position={[0, -0, -5]} />
         <spotLight position={[0, -0, 100]} angle={0.3} />
         <Keyboard />
-        <Spacebar />
       </Canvas>
     </>
   );
