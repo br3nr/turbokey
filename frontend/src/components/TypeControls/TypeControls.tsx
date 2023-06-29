704fc147c5338c7c732081be47d8f926f546c2d1
br3nr <repetitive_routine@protonmail.com>
2023-06-29 19:07:15 +0800

    Wrap target text words

--- frontend/src/components/TypeControls/TypeControls.tsx
+++ frontend/src/components/TypeControls/TypeControls.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Box, Center, Text, Input } from "@chakra-ui/react";
+import { Wrap, Box, Center, Text, Input } from "@chakra-ui/react";
 import ts from "typescript";
 import { SocketAddress } from "net";
 
@@ -14,20 +14,19 @@ function getWordList(sentence: string): string[] {
 
   for (let i = 0; i < words.length; i++) {
     if (i != words.length - 1) {
-      wordList[i] = words[i] + " "
-    }
-    else {
-      wordList[i] = words[i]
+      wordList[i] = words[i] + " ";
+    } else {
+      wordList[i] = words[i];
     }
   }
 
-  return wordList
+  return wordList;
 }
 
 export default function TypeControls() {
   const [curKeys, setCurKeys] = useState<string[]>([]);
-  const targetSentence = "Hello world, welcome to Turbo Key."
-  const wordList = getWordList(targetSentence)
+  const targetSentence = "Hello world, welcome to Turbo Key. Type out the words you can see!";
+  const wordList = getWordList(targetSentence);
   const inputRef = useRef(null);
 
   useEffect(() => {
@@ -60,17 +59,19 @@ export default function TypeControls() {
   return (
     <>
       <Center>
-        {targetSentence.split("").map((letter, index) => {
-          return letter === " " ? (
-            <Text color="green.500">&nbsp;</Text>
-          ) : letter === curKeys[index] ? (
-            <Text color="green.500">{letter}</Text>
-          ) : curKeys[index] === undefined ? (
-            <Text color="gray.500">{letter}</Text>
-          ) : (
-            <Text color="red.500">{letter}</Text>
-          );
-        })}
+        <Wrap width="350px"spacing="0px">
+          {targetSentence.split("").map((letter, index) => {
+            return letter === " " ? (
+              <Text color="green.500">&nbsp;</Text>
+            ) : letter === curKeys[index] ? (
+              <Text color="green.500">{letter}</Text>
+            ) : curKeys[index] === undefined ? (
+              <Text color="gray.500">{letter}</Text>
+            ) : (
+              <Text color="red.500">{letter}</Text>
+            );
+          })}
+        </Wrap>
       </Center>
       <Center height="100%">
         <div tabIndex={0} ref={inputRef}>
