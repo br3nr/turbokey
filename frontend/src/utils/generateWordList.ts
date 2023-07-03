27a4e2672626336a1e20875fe3ffbe3248ffa68a
br3nr <repetitive_routine@protonmail.com>
2023-07-03 19:59:43 +0800

    prevent duplicate words

--- frontend/src/utils/generateWordList.ts
+++ frontend/src/utils/generateWordList.ts
@@ -1,20 +1,28 @@
-import Papa from 'papaparse';
+import Papa from "papaparse";
 
 export async function generateWordList(): Promise<string> {
-
-	const response = await fetch('/wordlist/wordlist.csv');
+  const response = await fetch("/wordlist/wordlist.csv");
   const data = await response.text();
 
-	const parsedData = Papa.parse(data, { header: true }).data;
-	// @ts-ignore
-	const words = parsedData.map((row) => row.lemma);
-	var sentence = "";
+  const parsedData = Papa.parse(data, { header: true }).data;
+  // @ts-ignore
+  const words = parsedData.map((row) => row.lemma);
+  var sentence = "";
 
-	for(var i = 0; i < 20; i++) {
-		let randomIndex = Math.floor(Math.random() * words.length);
-		let randomWord = words[randomIndex];
-	
-		sentence += randomWord + " ";
-	}
+  var randomIndexes: number[] = [];
+
+  for (var i = 0; i < 20; i++) {
+    let randomIndex = Math.floor(Math.random() * words.length);
+
+    while (randomIndexes.includes(randomIndex)) {
+      randomIndex = Math.floor(Math.random() * words.length);
+    }
+
+    let randomWord = words[randomIndex];
+
+    randomIndexes.push(randomIndex);
+
+    sentence += randomWord + " ";
+  }
   return sentence;
 }
