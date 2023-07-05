cd90dcf565a5fba92dbf44a7ba45b94fe6204d34
br3nr <repetitive_routine@protonmail.com>
2023-07-05 19:50:39 +0800

    implement feedback when typed word exceeds limit

--- frontend/src/components/Word/Word.tsx
+++ frontend/src/components/Word/Word.tsx
@@ -4,8 +4,10 @@ import Keyboard from "../Keyboard/Keyboard";
 import { Text, Box } from "@chakra-ui/react";
 import styles from "./Word.module.css";
 import { Roboto_Mono } from "next/font/google";
+import { useEffect, useState } from "react";
+import React from "react";
 
-const robotoMono = Roboto_Mono({ subsets: ["latin"],  });
+const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 
 interface WordProps {
   targetWord: string;
@@ -13,13 +15,27 @@ interface WordProps {
 }
 
 const Word: React.FC<WordProps> = ({ targetWord, matchWord }) => {
+  const [refWord, setRefWord] = useState<string>(targetWord);
+
+  useEffect(() => {
+    if (matchWord && matchWord.length > targetWord.length) {
+      setRefWord(targetWord + matchWord.slice(targetWord.length));
+    } else {
+      setRefWord(targetWord);
+    }
+  }, [matchWord]);
+
   return (
     <>
       <Box margin={[0, 2, 0, 2]}>
         <div className={styles.word} style={robotoMono.style}>
-          {targetWord.split("").map((letter, index) => {
+          {refWord.split("").map((letter, index) => {
             return letter === " " ? (
               <span style={{ color: "red" }} key={index}>
+                {letter}
+              </span>
+            ) : index > targetWord.length - 1 ? (
+              <span style={{ color: "#8B0000" }} key={index}>
                 {letter}
               </span>
             ) : matchWord == undefined ? (
