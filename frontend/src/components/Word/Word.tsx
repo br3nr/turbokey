354428c988aab632a4046a070cd90d9777dd1d30
br3nr <repetitive_routine@protonmail.com>
2023-07-11 20:29:58 +0800

    Refactor Word component caret placement

--- frontend/src/components/Word/Word.tsx
+++ frontend/src/components/Word/Word.tsx
@@ -55,47 +55,44 @@ const Word: React.FC<WordProps> = ({ word, isFinalWord }) => {
           </div>
           <div>
             {refWord.split("").map((letter, index) => {
-              return index > word.targetWord.length - 1 ? (
-                <span style={{ color: "#8B0000" }} key={index}>
-                  {letter}
-                </span>
-              ) : word.typedWord == undefined ? (
+              return (
                 <>
-                  <span style={{ color: "gray" }} key={index}>
-                    {letter}
-                  </span>
-                </>
-              ) : letter === word.typedWord[index] ? (
-                <>
-                  <span style={{ color: "white" }} key={index}>
-                    {letter}
-                  </span>
-                  {isFinalWord && index == word.typedWord.length - 1 ? (
-                    <span className={styles.caret}></span>
-                  ) : (
-                    <></>
-                  )}
-                </>
-              ) : word.typedWord[index] == undefined ? (
-                <>
-                  <span style={{ color: "gray" }} key={index}>
-                    {letter}
-                  </span>
-                  {isFinalWord && index == word.typedWord.length - 1 ? (
-                    <span className={styles.caret}></span>
-                  ) : (
-                    <></>
-                  )}
-                </>
-              ) : word.typedWord === "" ? (
-                <>
-                  <span className={styles.caret}></span>
-                </>
-              ) : (
-                <>
-                  <span style={{ color: "red" }} key={index}>
-                    {letter}
-                  </span>
+                  <>
+                    {index > word.targetWord.length - 1 ? (
+                      <span style={{ color: "#8B0000" }} key={index}>
+                        {letter}
+                      </span>
+                    ) : word.typedWord == undefined ? (
+                      <>
+                        <span style={{ color: "gray" }} key={index}>
+                          {letter}
+                        </span>
+                      </>
+                    ) : letter === word.typedWord[index] ? (
+                      <>
+                        <span style={{ color: "white" }} key={index}>
+                          {letter}
+                        </span>
+                      </>
+                    ) : word.typedWord[index] == undefined ? (
+                      <>
+                        <span style={{ color: "gray" }} key={index}>
+                          {letter}
+                        </span>
+                      </>
+                    ) : word.typedWord === "" ? (
+                      <>
+                        <span className={styles.caret}></span>
+                      </>
+                    ) : (
+                      <>
+                        <span style={{ color: "red" }} key={index}>
+                          {letter}
+                        </span>
+                      </>
+                    )}
+                  </>
+                  <></>
                   {isFinalWord && index == word.typedWord.length - 1 ? (
                     <span className={styles.caret}></span>
                   ) : (
