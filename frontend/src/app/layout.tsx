7870f7495ade82fda32fae2e827dd5a555fabbbd
br3nr <repetitive_routine@protonmail.com>
2023-07-15 01:01:10 +0800

    Completely remove chakra

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,4 @@
 // app/layout.tsx
-import { Providers } from "./providers";
 
 export default function RootLayout({
   children,
@@ -9,9 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Providers>
           {children}
-        </Providers>
       </body>
     </html>
   );
