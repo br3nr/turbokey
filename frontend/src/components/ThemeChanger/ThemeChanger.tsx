714ac519f4cf4c6a420161763a0871fc7f981876
br3nr <repetitive_routine@protonmail.com>
2023-07-19 18:25:18 +0800

    Move dark & light buttons

--- frontend/src/components/ThemeChanger/ThemeChanger.tsx
+++ frontend/src/components/ThemeChanger/ThemeChanger.tsx
@@ -1,22 +1,31 @@
-
-import { useEffect, useState } from 'react';
-import { useTheme } from 'next-themes'
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 
 const ThemeChanger = () => {
-    const { theme, setTheme } = useTheme()
-    const [hasMounted, setHasMounted] = useState(false);
-  
-    useEffect(() => setHasMounted(true));
-    
-    // this line is the key to avoid the error.
-    if (!hasMounted) return null;
-  
-    return (
-      <div >
-        <button onClick={() => setTheme('light')}>Light Mode</button>
-        <button onClick={() => setTheme('dark')}>Dark Mode</button>
-      </div>
-    )
-  }
-  
-  export default ThemeChanger;
+  const { theme, setTheme } = useTheme();
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => setHasMounted(true));
+
+  // this line is the key to avoid the error.
+  if (!hasMounted) return null;
+
+  return (
+    <div className="flex justify-center items-center">
+      <button
+        className="text-1xl font-bold p-5"
+        onClick={() => setTheme("light")}
+      >
+        Light
+      </button>
+      <button
+        className="text-1xl font-bold p-5"
+        onClick={() => setTheme("dark")}
+      >
+        Dark
+      </button>
+    </div>
+  );
+};
+
+export default ThemeChanger;
