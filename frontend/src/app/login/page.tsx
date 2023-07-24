751a2f970517d0615281e7b1af321c102890ae62
br3nr <repetitive_routine@protonmail.com>
2023-07-24 20:54:13 +0800

    Basic login screen

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Page() {
+  
+    return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <h3>Welcome to Turbokey</h3>
+        <div className="flex items-center justify-center m-10">
+            <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+}
