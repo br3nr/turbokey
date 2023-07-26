c01e90a36720bb29c3990420d62b6e8b14dc6016
br3nr <repetitive_routine@protonmail.com>
2023-07-26 16:26:01 +0800

    Move authentication to index.js

--- bot/index.js
+++ bot/index.js
@@ -14,8 +14,42 @@ registerCommands(client);
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.sendFile("index.html", { root: "." });
+app.get("/", async (req, res) => {
+  const client_id = "1124947743190814740"; 
+  const client_secret = "";
+  const redirect_uri = "http://localhost:8000/";
+  const code = req.query.code;
+  fetch("https://discord.com/api/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `client_id=${client_id}&client_secret=${client_secret}&grant_type=authorization_code&code=${code}&redirect_uri=${redirect_uri}`,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const accessToken = data.access_token;
+
+      // Fetch the user data from the Discord API using the access token
+      fetch("https://discord.com/api/users/@me", {
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+      })
+        .then((result) => result.json())
+        .then((response) => {
+          const { username } = response;
+          res.send(`Hello ${username}`);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+
 });
 
 app.listen(port, () =>
