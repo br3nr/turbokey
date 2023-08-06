083ef2738bdba8ab0fd35714d20cd3465d016828
br3nr <repetitive_routine@protonmail.com>
2023-08-06 17:16:13 +0800

    Implement navbar template with route checking

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,20 @@
 // app/layout.tsx
+"use client";
 import "./globals.css";
 import Providers from "./Providers";
+import Navbar from "../components/Navbar/Navbar";
+import { usePathname } from "next/navigation";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
   return (
     <html lang="en">
       <body>
+        {pathname !== "/login" ? <Navbar /> : <></>}
         <Providers>{children}</Providers>
       </body>
     </html>
---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+
+export default function Navbar() {
+  return <div> Navbar </div>;
+}
