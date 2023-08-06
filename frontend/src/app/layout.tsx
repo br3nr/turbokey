82b5d8197de8f74038908a6f6adf4ab6931b2307
br3nr <repetitive_routine@protonmail.com>
2023-08-06 18:02:58 +0800

    Add username and avatar fetch to navbar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,17 +4,42 @@ import "./globals.css";
 import Providers from "./Providers";
 import Navbar from "../components/Navbar/Navbar";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [username, setUsername] = useState("");
+  const [avatar, setAvatar] = useState("");
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const response = await fetch("http://localhost:8000/auth/login", {
+        credentials: "include",
+      });
+      if (response.status === 200) {
+        const data = await response.json();
+        setUsername(data.username);
+        setAvatar(data.avatar_url);
+      }
+    };
+
+    if (pathname !== "/login") {
+      fetchUserData();
+    }
+  });
+
   const pathname = usePathname();
   return (
     <html lang="en">
       <body>
-        {pathname !== "/login" ? <Navbar /> : <></>}
+        {pathname !== "/login" ? (
+          <Navbar username={username} avatar={avatar} />
+        ) : (
+          <></>
+        )}
         <Providers>{children}</Providers>
       </body>
     </html>
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,23 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
-export default function Navbar() {
-  return <div> Navbar </div>;
+interface ProfileProps {
+  username: string;
+  avatar: string;
+}
+
+export default function Navbar(props: ProfileProps) {
+  return (
+    <div className="items-center justify-center flex w-1/2 m-auto bg-red-700">
+      <div className="flex items-center">ggg</div>
+      <div className="flex items-center ml-auto">
+        <Image
+          src={props.avatar}
+          width={50}
+          height={50}
+        />
+      </div>
+    </div>
+  );
 }
