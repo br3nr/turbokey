310e7f81227b68552530fe1caa550372c11584a3
br3nr <repetitive_routine@protonmail.com>
2023-08-10 21:20:09 +0800

    profile view

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -3,26 +3,43 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
+import Image from "next/image";
+
+
+//TODO make interface
 
 export default function Page() {
-  const [user, setUser] = useState("");
+  const [userData, setUserData] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       // check if cookie exists
       const cookie = Cookies.get();
       console.log(cookie);
-      const response = await fetch("http://localhost:8000/auth/login", { credentials: 'include' });
+      const response = await fetch("http://localhost:8000/auth/login", {
+        credentials: "include",
+      });
       const data = await response.json();
-      console.log(data);
+      setUserData(data);
     };
-		fetchData();
+    fetchData();
   }, []);
-
+  //https://tailwindcss.com/docs/customizing-colors
   return (
     <div>
-      <div className="m-auto">
-        <h3>Welcome {user}. You are now logged in.</h3>
+      <div className="items-center justify-center flex w-1/2 m-auto mt-10 bg-zinc-900 rounded-2xl">
+        <div className="flex items-center w-100 h-100 p-10">
+          <Image
+            src={userData.avatar_url}
+            width={100}
+            height={100}
+            className="rounded-full "
+          />
+        </div>
+        <div className="text-2xl">{userData.name}</div>
+        <div className="flex items-center ml-auto">
+
+        </div>
       </div>
     </div>
   );
