df66daf43efe958c8b4a9dac0d0e17e7fec71d42
br3nr <repetitive_routine@protonmail.com>
2023-11-19 17:55:59 +0800

    Implement oauth2 callback

--- go_bot/main.go
+++ go_bot/main.go
@@ -1,40 +1,89 @@
 package main
 
 import (
-  "fmt"
+	"golang.org/x/oauth2"
+  "github.com/joho/godotenv"
+  "os"
+	"io"
   "log"
-  "encoding/json"
-  "net/http"
+	"net/http"
+	"context"
+  "fmt"
 )
 
-type Article struct {
-  Title string `json:"Title"`
-  Desc string `json:"desc"`
-  Content string `json:"content"`
+const (
+  ScopeIdentify = "identify"
+  ScopeEmail = "email"
+)
 
-}
-
-type Articles []Article
-
-func allArticles(w http.ResponseWriter, r *http.Request) {
-  articles := Articles{
-    Article{Title:"Test title", Desc: "Test Desc", Content: "Test content"},
-  }
-
-  fmt.Println("Endpoint hit: All articles endpoint")
-  json.NewEncoder(w).Encode(articles)
+var Endpoint = oauth2.Endpoint{
+  AuthURL:   "https://discord.com/api/oauth2/authorize",
+	TokenURL:  "https://discord.com/api/oauth2/token",
+	AuthStyle: oauth2.AuthStyleInParams,
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Homepage Endpoint Hit")
 }
 
+func redirect(w http.ResponseWriter, r *http.Request) {
+	clientID := os.Getenv("BOT_CLIENT_ID")
+	clientSecret := os.Getenv("BOT_CLIENT_SECRET")
+
+  conf := oauth2.Config{
+    RedirectURL: "http://localhost:8081/auth/redirect", 
+    ClientID: clientID,
+    ClientSecret: clientSecret,
+    Scopes: []string{ScopeIdentify},
+    Endpoint: Endpoint,
+  }
+  
+
+  token, err := conf.Exchange(context.Background(), r.FormValue("code"))
+
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte(err.Error()))
+    return
+  }
+
+  res, err := conf.Client(context.Background(), token).Get("https://discord.com/api/users/@me")
+
+  if err != nil || res.StatusCode != 200 {
+    w.WriteHeader(http.StatusInternalServerError)
+    if err != nil {
+      w.Write([]byte(err.Error()))
+    } else {
+      w.Write([]byte(res.Status))
+    }
+  }
+
+  defer res.Body.Close()
+
+  body, err := io.ReadAll(res.Body)
+
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte(err.Error()))
+    return
+  }
+
+  w.Write(body)
+}
+
+
 func handleRequests() {
   http.HandleFunc("/", homePage)
-  http.HandleFunc("/articles", allArticles)
-  log.Fatal(http.ListenAndServe(":8081", nil))
+  http.HandleFunc("/auth/redirect", redirect)
+  log.Fatal(http.ListenAndServe(":8081", nil)) 
 }
 
 func main() {
-  handleRequests()
+  err := godotenv.Load()
+
+  if err != nil {
+    log.Fatal("Error loading .env")
+  } else {
+    handleRequests()
+  }
 }
