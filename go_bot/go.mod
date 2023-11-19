8b2dcaadd7dd66dd19baa11f587d62d695ca4426
br3nr <repetitive_routine@protonmail.com>
2023-11-19 21:12:10 +0800

    Semi working callback and login

--- go_bot/go.mod
+++ go_bot/go.mod
@@ -3,12 +3,16 @@ module turbokey.com/packages
 go 1.21.4
 
 require (
+	github.com/go-chi/chi v1.5.5
+	github.com/gorilla/sessions v1.2.2
 	github.com/joho/godotenv v1.5.1
+	github.com/rs/cors v1.10.1
 	golang.org/x/oauth2 v0.14.0
 )
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
 	golang.org/x/net v0.18.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
--- go_bot/main.go
+++ go_bot/main.go
@@ -1,89 +1,156 @@
 package main
 
 import (
-	"golang.org/x/oauth2"
-  "github.com/joho/godotenv"
-  "os"
-	"io"
-  "log"
-	"net/http"
 	"context"
-  "fmt"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
+	"golang.org/x/oauth2"
 )
 
 const (
-  ScopeIdentify = "identify"
-  ScopeEmail = "email"
+	ScopeIdentify = "identify"
+	ScopeEmail    = "email"
 )
 
+type DiscordUser struct {
+	id            string `json:"id"`
+	username      string `json:"username"`
+	avatar_url    string `json:"avatar"`
+	discriminator string `json:"discriminator"`
+}
+
+var store = sessions.NewCookieStore([]byte("your-secret-key"))
+
 var Endpoint = oauth2.Endpoint{
-  AuthURL:   "https://discord.com/api/oauth2/authorize",
+	AuthURL:   "https://discord.com/api/oauth2/authorize",
 	TokenURL:  "https://discord.com/api/oauth2/token",
 	AuthStyle: oauth2.AuthStyleInParams,
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Homepage Endpoint Hit")
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
 	clientID := os.Getenv("BOT_CLIENT_ID")
 	clientSecret := os.Getenv("BOT_CLIENT_SECRET")
 
-  conf := oauth2.Config{
-    RedirectURL: "http://localhost:8081/auth/redirect", 
-    ClientID: clientID,
-    ClientSecret: clientSecret,
-    Scopes: []string{ScopeIdentify},
-    Endpoint: Endpoint,
-  }
-  
+	conf := oauth2.Config{
+		RedirectURL:  "http://localhost:8000/auth/redirect",
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Scopes:       []string{ScopeIdentify},
+		Endpoint:     Endpoint,
+	}
 
-  token, err := conf.Exchange(context.Background(), r.FormValue("code"))
+	token, err := conf.Exchange(context.Background(), r.FormValue("code"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    w.Write([]byte(err.Error()))
-    return
-  }
+	res, err := conf.Client(context.Background(), token).Get("https://discord.com/api/users/@me")
+	if err != nil || res.StatusCode != http.StatusOK {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer res.Body.Close()
 
-  res, err := conf.Client(context.Background(), token).Get("https://discord.com/api/users/@me")
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-  if err != nil || res.StatusCode != 200 {
-    w.WriteHeader(http.StatusInternalServerError)
-    if err != nil {
-      w.Write([]byte(err.Error()))
-    } else {
-      w.Write([]byte(res.Status))
-    }
-  }
+	var user *DiscordUser
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-  defer res.Body.Close()
+	session, err := store.Get(r, "turbo-session")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-  body, err := io.ReadAll(res.Body)
+	session.Values["id"] = user.id
+	session.Values["discriminator"] = user.discriminator
+	session.Values["avatar_url"] = user.avatar_url
+	session.Values["username"] = user.username
 
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    w.Write([]byte(err.Error()))
-    return
-  }
+	err = session.Save(r, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-  w.Write(body)
+	http.Redirect(w, r, "http://localhost:3000", http.StatusFound)
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "turbo-session")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	user := DiscordUser{
+		id:            session.Values["id"].(string),
+		discriminator: session.Values["discriminator"].(string),
+		avatar_url:    session.Values["avatar_url"].(string),
+		username:      session.Values["username"].(string),
+	}
+
+	userJSON, err := json.Marshal(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(userJSON)
+}
 
 func handleRequests() {
-  http.HandleFunc("/", homePage)
-  http.HandleFunc("/auth/redirect", redirect)
-  log.Fatal(http.ListenAndServe(":8081", nil)) 
+	r := chi.NewRouter()
+
+	r.Get("/", homePage)
+	r.Get("/auth/redirect", redirect)
+	r.Get("/auth/login", login)
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowedMethods:   []string{"GET", "HEAD", "POST", "PUT", "OPTIONS"},
+	})
+
+	handler := c.Handler(r)
+
+	port := ":8000"
+	fmt.Printf("Server is running on port %s...\n", port)
+	err := http.ListenAndServe(port, handler)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-  err := godotenv.Load()
-
-  if err != nil {
-    log.Fatal("Error loading .env")
-  } else {
-    handleRequests()
-  }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env")
+	} else {
+		handleRequests()
+	}
 }
